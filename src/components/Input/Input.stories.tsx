e3c48cad39301766f499b99d54bc006e8c96a736
velopert <public.velopert@gmail.com>
2022-01-23 15:30:28 +0900

    Create Input components

---
+++ src/components/Input/Input.stories.tsx
@@ -0,0 +1,53 @@
+import { css } from '@emotion/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { palette } from '../../lib/palette'
+import Input from './Input'
+
+export default {
+  title: 'Input',
+  component: Input,
+} as ComponentMeta<typeof Input>
+
+type InputStory = ComponentStory<typeof Input>
+
+const Template: InputStory = (args) => <Input {...args} />
+
+export const Basic = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Basic.args = {
+  placeholder: 'Enter text...',
+}
+
+export const Size: InputStory = () => {
+  return (
+    <div css={wrapper}>
+      <Input size="sm" placeholder="Enter text..." />
+      <Input size="md" placeholder="Enter text..." />
+      <Input size="lg" placeholder="Enter text..." />
+    </div>
+  )
+}
+
+export const FocusedColor: InputStory = () => {
+  return (
+    <div css={wrapper}>
+      <Input focusedColor={palette.grey[900]} value="Black" />
+      <Input focusedColor="#2B7CFE" value="Blue" />
+    </div>
+  )
+}
+
+export const FullWidth: InputStory = () => {
+  return <Input isFullWidth placeholder="Full Width" />
+}
+
+export const Disabled: InputStory = () => {
+  return <Input disabled placeholder="Disabled" />
+}
+
+const wrapper = css`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  align-items: flex-start;
+`
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,126 @@
+import { css } from '@emotion/react'
+import { InputHTMLAttributes, useState } from 'react'
+import { palette } from '../../lib/palette'
+
+type InputSize = 'sm' | 'md' | 'lg'
+
+interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  /**
+   * Default size of the input is `md`
+   */
+  size?: InputSize
+  /**
+   * Color of the border and text when input is foucused.
+   * You can also override this color from VeloProvider.
+   */
+  focusedColor?: string
+
+  /**
+   * Set this to `true` when you want to match the size with its parent element.
+   */
+  isFullWidth?: boolean
+}
+
+function Input({
+  size = 'md',
+  focusedColor = palette.teal[500],
+  isFullWidth,
+  disabled,
+  ...rest
+}: InputProps) {
+  const [focused, setFocused] = useState(false)
+
+  return (
+    <div css={wrapper(isFullWidth)}>
+      <div
+        css={[
+          inputBox({ size, disabled, isFullWidth }),
+          focused && focusedStyle(focusedColor),
+        ]}
+      >
+        <input
+          css={inputStyle}
+          onFocus={(e) => {
+            rest.onFocus?.(e)
+            setFocused(true)
+          }}
+          onBlur={(e) => {
+            rest.onBlur?.(e)
+            setFocused(false)
+          }}
+          disabled={disabled}
+          {...rest}
+        />
+      </div>
+    </div>
+  )
+}
+
+const sizes = {
+  sm: '0.875rem',
+  md: '1rem',
+  lg: '1.125rem',
+}
+
+interface InputOption {
+  size: InputSize
+  isFullWidth?: boolean
+  disabled?: boolean
+}
+
+const wrapper = (isFullWidth?: boolean) => css`
+  display: inline-flex;
+  ${isFullWidth &&
+  css`
+    width: 100%;
+  `}
+`
+
+const inputBox = ({ size, disabled }: InputOption) => css`
+  width: 100%;
+  border: 1px solid ${palette.grey[300]};
+  font-size: ${sizes[size]};
+  transition: 0.125s all ease-in;
+  ${palette.grey[800]};
+  display: inline-flex;
+  border-radius: 0.25rem;
+  backgruond: white;
+
+  ${disabled &&
+  css`
+    background: ${palette.grey[100]};
+    color: ${palette.grey[400]};
+    cursor: not-allowed;
+    input {
+      cursor: not-allowed;
+    }
+  `}
+`
+
+const focusedStyle = (color: string) => css`
+  color: ${color};
+  border-color: ${color};
+`
+
+const inputStyle = css`
+  flex: 1;
+  font-size: 1em;
+  padding: 0;
+  outline: none;
+  height: 2.5em;
+  padding-left: 0.875em;
+  padding-right: 0.875em;
+  background: transparent;
+
+  color: inherit;
+  border: none;
+
+  font-family: inherit;
+
+  &::placeholder {
+    color: ${palette.grey[400]};
+  }
+`
+
+export default Input
