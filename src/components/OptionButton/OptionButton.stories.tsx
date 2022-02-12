3aaefadf4799029352e3fdd5b42e7d7ad5d910d6
velopert <public.velopert@gmail.com>
2022-02-12 18:26:38 +0900

    Creates OptionButton

---
+++ src/components/OptionButton/OptionButton.stories.tsx
@@ -0,0 +1,269 @@
+import { css } from '@emotion/react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { useEffect, useState } from 'react'
+import Button from '../Button'
+import Icon from '../Icon'
+import OptionButton from './OptionButton'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'OptionButton',
+  component: OptionButton,
+} as ComponentMeta<typeof OptionButton>
+
+const Template: ComponentStory<typeof OptionButton> = (args) => {
+  const [checked, setChecked] = useState(false)
+
+  useEffect(() => {
+    setChecked(!!args.checked)
+  }, [args.checked])
+
+  return (
+    <div>
+      <OptionButton
+        {...args}
+        checked={checked}
+        onChangeValue={() => setChecked(true)}
+      />
+      <div css={{ marginTop: '1rem' }}>
+        <Button onClick={() => setChecked(false)}>Reset</Button>
+      </div>
+    </div>
+  )
+}
+
+export const Basic = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Basic.args = {
+  children: 'Option 1',
+}
+
+Basic.argTypes = {}
+
+export function MultipleItems() {
+  const [value, setValue] = useState(1)
+
+  return (
+    <div css={wrapper}>
+      <OptionButton value={1} onChangeValue={setValue} checked={value === 1}>
+        Option 1
+      </OptionButton>
+      <OptionButton value={2} checked={value === 2} onChangeValue={setValue}>
+        Option 2
+      </OptionButton>
+      <OptionButton value={3} checked={value === 3} onChangeValue={setValue}>
+        Option 3
+      </OptionButton>
+    </div>
+  )
+}
+
+export function Disabled() {
+  const [value, setValue] = useState(1)
+
+  return (
+    <div css={wrapper}>
+      <OptionButton
+        value={1}
+        onChangeValue={setValue}
+        checked={value === 1}
+        disabled
+      >
+        Option 1
+      </OptionButton>
+      <OptionButton
+        value={2}
+        checked={value === 2}
+        onChangeValue={setValue}
+        disabled
+      >
+        Option 2
+      </OptionButton>
+      <OptionButton
+        value={3}
+        checked={value === 3}
+        onChangeValue={setValue}
+        disabled
+      >
+        Option 3
+      </OptionButton>
+    </div>
+  )
+}
+
+export function FillOnChecked() {
+  const [value, setValue] = useState(1)
+
+  return (
+    <div css={wrapper}>
+      <OptionButton
+        value={1}
+        onChangeValue={setValue}
+        checked={value === 1}
+        fillOnChecked
+      >
+        Option 1
+      </OptionButton>
+      <OptionButton
+        value={2}
+        checked={value === 2}
+        onChangeValue={setValue}
+        fillOnChecked
+      >
+        Option 2
+      </OptionButton>
+      <OptionButton
+        value={3}
+        checked={value === 3}
+        onChangeValue={setValue}
+        fillOnChecked
+      >
+        Option 3
+      </OptionButton>
+    </div>
+  )
+}
+
+export function BorderOption() {
+  const [value, setValue] = useState(1)
+
+  return (
+    <div css={noMarginWrapper}>
+      <OptionButton
+        value={1}
+        onChangeValue={setValue}
+        checked={value === 1}
+        borderOption="left"
+      >
+        Option 1
+      </OptionButton>
+      <OptionButton
+        value={2}
+        checked={value === 2}
+        onChangeValue={setValue}
+        borderOption="middle"
+      >
+        Option 2
+      </OptionButton>
+      <OptionButton
+        value={3}
+        checked={value === 3}
+        onChangeValue={setValue}
+        borderOption="right"
+      >
+        Option 3
+      </OptionButton>
+    </div>
+  )
+}
+
+export function BorderOptionFilled() {
+  const [value, setValue] = useState(1)
+
+  return (
+    <div css={noMarginWrapper}>
+      <OptionButton
+        value={1}
+        onChangeValue={setValue}
+        checked={value === 1}
+        borderOption="left"
+        fillOnChecked
+      >
+        Option 1
+      </OptionButton>
+      <OptionButton
+        value={2}
+        checked={value === 2}
+        onChangeValue={setValue}
+        borderOption="middle"
+        fillOnChecked
+      >
+        Option 2
+      </OptionButton>
+      <OptionButton
+        value={3}
+        checked={value === 3}
+        onChangeValue={setValue}
+        borderOption="right"
+        fillOnChecked
+      >
+        Option 3
+      </OptionButton>
+    </div>
+  )
+}
+
+export function Size() {
+  const [value, setValue] = useState(1)
+
+  return (
+    <div css={wrapper}>
+      <OptionButton
+        value={1}
+        onChangeValue={setValue}
+        checked={value === 1}
+        size="sm"
+      >
+        Option 1
+      </OptionButton>
+      <OptionButton
+        value={2}
+        checked={value === 2}
+        onChangeValue={setValue}
+        size="md"
+      >
+        Option 2
+      </OptionButton>
+      <OptionButton
+        value={3}
+        checked={value === 3}
+        onChangeValue={setValue}
+        size="lg"
+      >
+        Option 3
+      </OptionButton>
+    </div>
+  )
+}
+
+export function WithIcon() {
+  const [value, setValue] = useState(1)
+
+  return (
+    <div css={noMarginWrapper}>
+      <OptionButton
+        value={1}
+        onChangeValue={setValue}
+        checked={value === 1}
+        icon={<Icon name="globe" />}
+        borderOption="left"
+        fillOnChecked
+      >
+        Public
+      </OptionButton>
+      <OptionButton
+        value={2}
+        checked={value === 2}
+        onChangeValue={setValue}
+        icon={<Icon name="lock" />}
+        borderOption="right"
+        fillOnChecked
+      >
+        Private
+      </OptionButton>
+    </div>
+  )
+}
+
+const wrapper = css`
+  display: flex;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+  & + & {
+    margin-top: 0.5rem;
+  }
+`
+
+const noMarginWrapper = css`
+  display: flex;
+`
---
+++ src/components/OptionButton/OptionButton.tsx
@@ -0,0 +1,187 @@
+import { css } from '@emotion/react'
+import React, { ReactNode } from 'react'
+import { palette } from '../../lib/palette'
+import { Size, sizeSets } from '../../lib/sizes'
+import { rgba } from 'polished'
+
+interface Props<T> {
+  checked?: boolean
+  size?: Size
+  children?: ReactNode
+  onChangeValue?(value?: T): void
+  value?: T
+  fillOnChecked?: boolean
+  borderOption?: 'default' | 'left' | 'middle' | 'right'
+  disabled?: boolean
+  /**
+   * Icon is shown at the left side of the components. Use SVG icon.
+   */
+  icon?: React.ReactNode
+}
+
+/**
+ * `OptionButton` is used to select one of the item in a group. It is similar to `Radio` but it appears as a button.
+ * @returns
+ */
+function OptionButton<T>({
+  size = 'md',
+  checked,
+  children,
+  onChangeValue,
+  value,
+  fillOnChecked,
+  borderOption = 'default',
+  icon,
+  disabled,
+}: Props<T>) {
+  return (
+    <button
+      className={`option-button ${borderOption} ${checked ? 'checked' : ''}`}
+      css={block(size, checked, fillOnChecked, borderOption)}
+      onClick={() => {
+        onChangeValue?.(value)
+      }}
+      disabled={disabled}
+    >
+      {icon && <div css={iconWrapper}>{icon}</div>}
+      {children}
+    </button>
+  )
+}
+
+const block = (
+  size: Size,
+  checked: boolean | undefined,
+  fillOnChecked: boolean | undefined,
+  borderOption: 'default' | 'left' | 'middle' | 'right'
+) => css`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  font-size: ${sizeSets[size].fontSize};
+  height: ${sizeSets[size].height};
+
+  border: none;
+  outline: none;
+  border: 1px solid ${palette.grey[500]};
+  color: ${palette.grey[500]};
+  border-radius: 0.25rem;
+  padding-left: 1em;
+  padding-right: 1em;
+  transition: 0.125s color ease-in, 0.125s background ease-in,
+    0.125s border-color ease-in;
+  cursor: pointer;
+  font-family: inherit;
+  font-weight: 600;
+  &:hover:enabled {
+    color: ${palette.teal[500]};
+  }
+  ${checked &&
+  css`
+    color: ${palette.teal[500]};
+    border-color: ${palette.teal[500]};
+    &:hover:enabled {
+      color: ${rgba(palette.teal[500], 0.8)};
+      border-color: ${rgba(palette.teal[500], 0.8)};
+    }
+
+    ${fillOnChecked &&
+    css`
+      background: ${palette.teal[500]};
+      color: white;
+      &:hover:enabled {
+        color: white;
+        background: ${rgba(palette.teal[500], 0.8)};
+        border-color: ${rgba(palette.teal[500], 0.8)};
+      }
+    `}
+  `}
+
+  ${borderOption !== 'default' &&
+  css`
+    .option-button + & {
+      border-left: 1px solid ${palette.grey[500]};
+    }
+
+    .checked + & {
+      border-left-width: 0px;
+    }
+  `}
+
+  ${borderOption === 'left' &&
+  css`
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    border-right-width: 0;
+    ${checked &&
+    css`
+      border-right-width: 1px;
+    `}/* ${checked &&
+    css`
+      box-shadow: 1px 0px 0px 0px ${palette.teal[500]};
+    `} */
+  `}
+
+  ${borderOption === 'right' &&
+  css`
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-left: none;
+    ${checked &&
+    css`
+      border-left: 1px solid ${palette.teal[500]} !important;
+      &:hover:enabled {
+        border-color: ${rgba(palette.teal[500], 0.8)} !important;
+      }
+    `}/* box-shadow: -1px 0px 0px 0px ${palette.grey[500]};
+    ${checked &&
+    css`
+      box-shadow: -1px 0px 0px 0px ${palette.teal[500]};
+    `} */
+  `}
+
+  ${borderOption === 'middle' &&
+  css`
+    border-left-width: 0;
+    border-right-width: 0;
+    border-radius: 0;
+    ${checked &&
+    css`
+      border-left: 1px solid ${palette.teal[500]} !important;
+      border-right-width: 1px;
+      &:hover:enabled {
+        border-color: ${rgba(palette.teal[500], 0.8)} !important;
+      }
+    `}/* box-shadow: -1px 0px 0px 0px ${palette.grey[500]},
+      1px 0px 0px 0px ${palette.grey[500]};
+    ${checked &&
+    css`
+      box-shadow: -1px 0px 0px 0px ${palette.teal[500]},
+        1px 0px 0px 0px ${palette.teal[500]};
+    `} */
+  `} /* ${checked &&
+  css`
+    z-index: 5;
+  `} */
+
+  &:disabled {
+    filter: grayscale(25%);
+    cursor: not-allowed;
+    opacity: 0.4;
+  }
+`
+
+const iconWrapper = css`
+  margin-right: 1em;
+  display: flex;
+  svg {
+    color: inherit;
+    display: block;
+    width: 1.25em;
+    height: 1.25em;
+  }
+  margin-right: 0.75em;
+`
+
+export default OptionButton
