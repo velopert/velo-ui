e811ccc892a6358d353312adfca8199ef14bd483
velopert <public.velopert@gmail.com>
2022-02-19 16:14:51 +0900

    Initializes Text component

---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,119 @@
+import { css } from '@emotion/react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Text from './Text'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Text',
+  component: Text,
+} as ComponentMeta<typeof Text>
+
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />
+
+export const Basic = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Basic.args = {
+  children:
+    'The quick brown fox jumps over the lazy dog. 다람쥐 헌 쳇바퀴에 타고파.',
+  size: 32,
+  weight: 500,
+}
+
+Basic.argTypes = {}
+
+export function Size() {
+  const text =
+    'The quick brown fox jumps over the lazy dog. 다람쥐 헌 쳇바퀴에 타고파.'
+  return (
+    <div css={wrapper}>
+      <Text size={72}>{text}</Text>
+      <Text size={56}>{text}</Text>
+      <Text size={48}>{text}</Text>
+      <Text size={40}>{text}</Text>
+      <Text size={32}>{text}</Text>
+      <Text size={24}>{text}</Text>
+      <Text size={20}>{text}</Text>
+      <Text size={16}>{text}</Text>
+      <Text size={14}>{text}</Text>
+      <Text size={12}>{text}</Text>
+      <Text size={10}>{text}</Text>
+    </div>
+  )
+}
+
+export function Weight() {
+  const text =
+    'The quick brown fox jumps over the lazy dog. 다람쥐 헌 쳇바퀴에 타고파.'
+  return (
+    <div css={wrapper}>
+      <Text size={32} weight={900}>
+        {text}
+      </Text>
+      <Text size={32} weight={800}>
+        {text}
+      </Text>
+      <Text size={32} weight={700}>
+        {text}
+      </Text>
+      <Text size={32} weight={600}>
+        {text}
+      </Text>
+      <Text size={32} weight={500}>
+        {text}
+      </Text>
+      <Text size={32} weight={400}>
+        {text}
+      </Text>
+      <Text size={32} weight={300}>
+        {text}
+      </Text>
+      <Text size={32} weight={200}>
+        {text}
+      </Text>
+      <Text size={32} weight={100}>
+        {text}
+      </Text>
+    </div>
+  )
+}
+
+export function Align() {
+  const text = 'Hello World'
+  return (
+    <div css={wrapper}>
+      <Text align="left">{text}</Text>
+      <Text align="center">{text}</Text>
+      <Text align="right">{text}</Text>
+    </div>
+  )
+}
+
+export function Truncate() {
+  const text =
+    'The quick brown fox jumps over the lazy dog. 다람쥐 헌 쳇바퀴에 타고파.'
+  return (
+    <div css={[wrapper, { width: '150px' }]}>
+      <Text truncate>{text}</Text>
+    </div>
+  )
+}
+
+export function Clamp() {
+  const text =
+    'The quick brown fox jumps over the lazy dog. 다람쥐 헌 쳇바퀴에 타고파.'
+  return (
+    <div css={[wrapper, { width: '150px' }]}>
+      <Text clamp={2}>{text}</Text>
+    </div>
+  )
+}
+
+const wrapper = css`
+  display: flex;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+  flex-direction: column;
+  & + & {
+    margin-top: 0.5rem;
+  }
+`
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,81 @@
+import { jsx } from '@emotion/core'
+import { css } from '@emotion/react'
+import React, { useMemo } from 'react'
+import { addSyntheticLeadingComment } from 'typescript'
+
+interface Props {
+  as?:
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'p'
+    | 'span'
+    | 'div'
+    | 'label'
+    | 'strong'
+  whiteSpace?: 'pre' | 'nowrap' | 'pre-wrap' | 'pre-line' | 'normal'
+  children: React.ReactNode
+  size?: 10 | 12 | 14 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 72
+  weight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
+  align?: 'left' | 'center' | 'right'
+  className?: string
+  truncate?: boolean
+  clamp?: number
+}
+
+function Text({
+  as = 'div',
+  whiteSpace,
+  children,
+  size = 16,
+  weight = 400,
+  align,
+  className,
+  truncate,
+  clamp,
+}: Props) {
+  const fontSize = useMemo(() => `${size / 16}rem`, [size])
+
+  return jsx(
+    as,
+    {
+      css: [
+        css({
+          whiteSpace,
+          fontSize,
+          fontWeight: weight,
+          textAlign: align,
+        }),
+        textStyle,
+        truncate && truncateStyle,
+        clamp && clampStyle(clamp),
+      ],
+      className,
+    },
+    children
+  )
+}
+
+const textStyle = css`
+  margin: 0;
+  line-height: 1.5;
+`
+
+const truncateStyle = css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 100%;
+`
+
+const clampStyle = (lines: number) => css`
+  display: -webkit-box;
+  -webkit-line-clamp: ${lines};
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`
+
+export default Text
