ac2094f70292f96e4f0e085f3e54bf24184d576e
velopert <public.velopert@gmail.com>
2022-04-21 04:23:58 +0900

    feat: setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+import external from 'rollup-plugin-peer-deps-external'
+import svgr from '@svgr/rollup'
+import url from 'rollup-plugin-url'
+
+const packageJson = require('./package.json')
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      name: 'react-lib',
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    url(),
+    svgr(),
+    terser(),
+  ],
+}
---
+++ src/typings.ts
@@ -0,0 +1,10 @@
+declare module '*.svg' {
+  import * as React from 'react'
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >
+
+  const src: string
+  export default src
+}
