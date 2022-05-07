8496f8f2c5afc9d94a6a3b635cb40bda137897bc
velopert <public.velopert@gmail.com>
2022-05-08 03:03:27 +0900

    [publish-alpha] missing type

--- rollup.config.js
+++ rollup.config.js
@@ -6,31 +6,45 @@ import { terser } from 'rollup-plugin-terser'
 import external from 'rollup-plugin-peer-deps-external'
 import svgr from '@svgr/rollup'
 import url from 'rollup-plugin-url'
+import dts from 'rollup-plugin-dts'
 
 const packageJson = require('./package.json')
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-      name: 'react-lib',
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    commonjs(),
-    typescript(),
-    url(),
-    svgr(),
-    terser(),
-  ],
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        name: 'react-lib',
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript(),
+      url(),
+      svgr(),
+      terser(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: packageJson.types, format: 'es' }],
+    plugins: [
+      url(),
+      svgr({
+        typescript: true,
+      }),
+      dts(),
+    ],
+  },
+]
